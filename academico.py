23d32bd31a2ba176f08394cfa5263f7ce75312be
Oscar O. Vargas <os2varpla@gmail.com>
2018-08-21 20:20:58 -0500

    practica

---
+++ academico.py
@@ -0,0 +1,18 @@
+sumaedades=0
+promedio=0
+edad1=15
+persona1="Juan Carlos Arteaga"
+edad2=18
+persona2="Angela Maria Lopez"
+edad3=22
+persona3="Roberto Andres Jurado"
+edad4=19
+persona4="Monica Maribel Hurtado"
+print("SISTEMA DE INFORMACIÓN ACADEMICO - PROMEDIO DE EDADES")
+sumaedades=edad1+edad2+edad3+edad4
+print("El estudiante",persona1, " tiene " ,edad1, " años ")
+print("El estudiante",persona2, " tiene " ,edad2, " años ")
+print("El estudiante",persona3, " tiene " ,edad3, " años ")
+print("El estudiante",persona4, " tiene " ,edad4, " años ")
+promedio=sumaedades/4
+print("el promedio de edades es: " ,promedio)      
---
+++ calcv1.py
@@ -0,0 +1,16 @@
+n1=10
+n2=5
+print("calculadora básica v1.0")
+print("primer número es:",n1)
+print("segundo número es:",n2)
+suma=n1+n2
+resta=n1-n2
+multiplicación=n1*n2
+división=n1/n2
+print("**********************************")
+print("RESULTADOS")
+print("**********************************")
+print("la suma es:",suma)
+print("la resta es:",resta)
+print("la multiplicación es:",multiplicación)
+print("la división es:",división)
