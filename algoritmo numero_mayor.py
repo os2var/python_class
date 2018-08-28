026d4f47eb3265d6dc9fa0e09af445a9a466b36a
Oscar O. Vargas <os2varpla@gmail.com>
2018-08-27 21:19:50 -0500

    practica2

---
+++ algoritmo numero_mayor.py
@@ -0,0 +1,9 @@
+#Algoritmo numero_mayor
+n1=0
+n2=0
+n1=input ("Ingrese el primer numero:")
+n2=input("Ingrese el segundo numero:")
+if n1>n2 :
+	print ("el numero mayor es:" ,n1)
+else:
+	print ("el numero mayor es :", n2)
