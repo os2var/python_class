f02b53a07042e03db72bcf764151adc7edfaf697
Oscar O. Vargas <os2varpla@gmail.com>
2018-09-04 23:23:32 -0500

    ok

---
+++ ciclo_3.py
@@ -0,0 +1,7 @@
+rango=1
+rangui=int(input("ingrese el rango de tabla:..."))
+tabla=int(input("ingrese el numero de la tabla a desarrollar:..."))
+while rango<=rangui :
+	print(tabla, "*", rango, "=" , tabla*rango)
+	rango = rango+1
+	print("___________________")
