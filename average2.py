0523548580a8f5e466047f7f092d1e09393f39e9
Oscar O. Vargas <os2varpla@gmail.com>
2018-09-12 00:52:26 -0500

    hoy

--- average2.py
+++ average2.py
@@ -1,13 +1,29 @@
-i=1
-suma_edades=0
-max_personas=int(input("ingrese cantidad de personas:.."))
-while i<=max_personas:
-	edad=int(input("ingrese la edad"))
-	i = i+1
-	suma_edades=suma_edades + edad
-
-promedio_edades = suma_edades/max_personas
-print(float(promedio_edades))	
-
-
-
+cant=int(input("ingresar cantidad de personas"))
+c=1
+gmujer=0
+ghombre=0
+edusuario=0
+edmujer=0
+edhombre=0
+while c<=cant:
+    edad=float(input("ingrese su edad"))
+    genero=input("ingrese su genero")
+    edusuario=edusuario+edad
+    c=c+1
+    promedio=edusuario/cant
+    if genero=="m":
+        ghombre=ghombre+1
+        edhombre=edhombre+edad
+    elif genero=="f":
+         gmujer=gmujer+1
+         edmujer=edmujer+edad
+        
+promehom=edhombre/ghombre
+promujer=edmujer/gmujer                      
+print("MENU PRINCIPAL")
+print("1.promedio de todas las edades",promedio)
+print("2.numero total de mujeres",gmujeres)
+print("3.numero total de hombres",ghombres)
+print("4.promedio de edades de mujeres",promediomuj)
+print("5.promedio de edades de hombres",promediohom)
+print("numero total de personas ingresadas en el sistema",cant)
